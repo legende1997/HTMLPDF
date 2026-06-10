411ccb9a81a06163261ceec0e37c1b54bafbb7a7
legende1997 <lcardoso27@icloud.com>
2026-06-10 15:07:48 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const fetch = require('node-fetch');
 const { PDFDocument } = require('pdf-lib');
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 app.use(express.json({ limit: '10mb' }));
 
 app.get('/', (req, res) => {
